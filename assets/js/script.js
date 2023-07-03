be292157b81bfaf9581b0d9b8817d9d4921909d9
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-07-03 15:35:49 -0500

    Add display book list function

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,30 @@ function removeBook(id) {
   books = bookData;
   localStorage.setItem('bookData', JSON.stringify(books));
 }
+
+function displaylist() {
+  books.forEach((book) => {
+    const bookCard = document.createElement('li');
+    const removeButton = document.createElement('button');
+    const hr = document.createElement('hr');
+
+    bookCard.innerHTML += `    
+    <p>${book.title}</p>
+    <p>${book.author}</p>  
+       `;
+    bookCard.id = `data-${book.id}`;
+    removeButton.textContent = 'Remove';
+    removeButton.dataset.id = book.id;
+    removeButton.addEventListener('click', (e) => {
+      const { id } = e.target.dataset;
+      removeBook(id);
+      const bookEl = document.getElementById(`data-${book.id}`);
+      bookList.removeChild(bookEl);
+      window.location.reload();
+    });
+    bookCard.appendChild(removeButton);
+    bookCard.appendChild(hr);
+    bookList.appendChild(bookCard);
+  });
+}
+displaylist();
