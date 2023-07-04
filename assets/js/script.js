922439ba0765daafc286eb332c825099ac70ff36
diarisdiakite <diarisdiakite@gmail.com>
2023-07-04 21:52:37 +0200

    Create Book class. Change functions to class methods

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,58 +5,58 @@ const formTitle = document.getElementById('title');
 const formAuthor = document.getElementById('author');
 const bookList = document.querySelector('#list');
 
-function addBook(title, author) {
-  if (title !== '' && author !== '') {
-    const book = {
-      id: books.length + 1,
-      title,
-      author,
-    };
 
-    books.push(book);
+class Book {
+  constructor() {
+    this.title = document.getElementById('title');
+    this.author = document.getElementById('author');
+  }
+
+  addBook(title, author) {
+    if (title !== '' && author !== '') {
+      const book = {
+        id: books.length + 1,
+        title,
+        author,
+      };
+
+      books.push(book);
+      localStorage.setItem('bookData', JSON.stringify(books));
+    }
+  }
+
+  removeBook(id) {
+    let bookData = JSON.parse(localStorage.getItem('bookData'));
+    bookData = bookData.filter((local) => local.id !== parseInt(id, 10));
+    books = bookData;
     localStorage.setItem('bookData', JSON.stringify(books));
   }
-}
 
-addButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  addBook(formTitle.value, formAuthor.value);
+  displaylist() {
+    books.forEach((book) => {
+      const bookCard = document.createElement('li');
+      const removeButton = document.createElement('button');
+      const hr = document.createElement('hr');
 
-  formTitle.value = '';
-  formAuthor.value = '';
-  window.location.reload();
-});
-
-function removeBook(id) {
-  let bookData = JSON.parse(localStorage.getItem('bookData'));
-  bookData = bookData.filter((local) => local.id !== parseInt(id, 10));
-  books = bookData;
-  localStorage.setItem('bookData', JSON.stringify(books));
-}
-
-function displaylist() {
-  books.forEach((book) => {
-    const bookCard = document.createElement('li');
-    const removeButton = document.createElement('button');
-    const hr = document.createElement('hr');
-
-    bookCard.innerHTML += `    
-    <p>${book.title}</p>
-    <p>${book.author}</p>  
-       `;
-    bookCard.id = `data-${book.id}`;
-    removeButton.textContent = 'Remove';
-    removeButton.dataset.id = book.id;
-    removeButton.addEventListener('click', (e) => {
-      const { id } = e.target.dataset;
-      removeBook(id);
-      const bookEl = document.getElementById(`data-${book.id}`);
-      bookList.removeChild(bookEl);
-      window.location.reload();
+      bookCard.innerHTML += `    
+      <p>${book.title}</p>
+      <p>${book.author}</p>  
+        `;
+      bookCard.id = `data-${book.id}`;
+      removeButton.textContent = 'Remove';
+      removeButton.dataset.id = book.id;
+      removeButton.addEventListener('click', (e) => {
+        const { id } = e.target.dataset;
+        this.removeBook(id);
+        const bookEl = document.getElementById(`data-${book.id}`);
+        bookList.removeChild(bookEl);
+        window.location.reload();
+      });
+      bookCard.appendChild(removeButton);
+      bookCard.appendChild(hr);
+      bookList.appendChild(bookCard);
     });
-    bookCard.appendChild(removeButton);
-    bookCard.appendChild(hr);
-    bookList.appendChild(bookCard);
-  });
+  }
+
 }
 displaylist();
