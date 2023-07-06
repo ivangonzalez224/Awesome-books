7d100ff461e32534cec69408923e0fa2896f9de0
diarisdiakite <diarisdiakite@gmail.com>
2023-07-06 04:05:04 +0200

    Add Contact and Footer sections. Add navlinks EventListener skeletons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,35 +72,26 @@ addButton.addEventListener('click', (e) => {
 });
 
 
-let displayedDiv = document.createElement('displayed-section');
 
-let displayBooks = document.getElementById('display-list');
-let navbarDisplaylink = document.getElementById('topNav_listItem');
+const displayBooks = document.getElementById('display-list');
+const navbarDisplaylink = document.getElementById('topNav_listItem');
 
-let displayAddBook = document.getElementById('addBook-div');
-let navbarAddlink = document.getElementById('topNav_newItem');
+const displayAddBook = document.getElementById('add-book-div');
+const navbarAddlink = document.getElementById('topNav_newItem');
 
-let displayContact = document.getElementById('contact-div');
-let navbarContactlink = document.getElementById('topNav_contactItem');
+const displayContact = document.getElementById('contact-div');
+const navbarContactlink = document.getElementById('topNav_contactItem');
+
+const date = document.getElementById('date');
+date.innerHTML = `<small>${Date()}</small>`;
+
   
 navbarDisplaylink.addEventListener('click', () => {
-  displayBooks.classList.remove('hidden');
-  displayAddBook.classList.add('hidden');
-  displayContact.classList.add('hidden');
-  displayedDiv.appendChild(displayBooks);
 })
 
 navbarAddlink.addEventListener('click', () => {
-  displayAddBook.classList.remove('hidden');
-  displayBooks.classList.add('hidden');
-  displayContact.classList.add('hidden');
-  displayedDiv.appendChild(displayAddBook);
 })
 
 navbarContactlink.addEventListener('click', () => {
-  displayContact.classList.remove('hidden');
-  displayAddBook.classList.add('hidden');
-  displayBooks.classList.add('hidden');
-  displayedDiv.appendChild(displayContact);
 })
 
